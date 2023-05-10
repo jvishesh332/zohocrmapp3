6adbad5388d3335ce6e510ea695f7bcae40eae3b
vishesh <jvishesh23@gmail.com>
2023-05-10 12:10:49 +0530

    zohocrmapp3

---
+++ src/main/java/com/zohocrm/controller/ContactController.java
@@ -0,0 +1,36 @@
+	package com.zohocrm.controller;
+	
+	import java.util.List;
+	
+	import org.springframework.stereotype.Controller;
+	import org.springframework.ui.Model;
+	import org.springframework.ui.ModelMap;
+	import org.springframework.web.bind.annotation.RequestMapping;
+	
+	import com.zohocrm.entities.Contact;
+	import com.zohocrm.entities.Lead;
+	import com.zohocrm.services.ContactService;
+	
+	@Controller
+	public class ContactController {
+		
+		
+		private ContactService contactService;
+		
+		
+		
+		public ContactController(ContactService contactService) {
+			this.contactService = contactService;
+		}
+	
+	
+	
+		@RequestMapping("/listallcontacts")
+		public String listAllLeads(Model model) {
+			List<Contact> contacts = contactService.getAllContacts();
+			model.addAttribute("contacts",contacts);
+			return "list_contacts";
+		}
+		
+		
+	}
---
+++ src/main/webapp/WEB-INF/jsps/generate_bill.jsp
@@ -0,0 +1,29 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+        <%@ include file ="menu.jsp" %>
+    
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>create bill</title>
+</head>
+<body>
+  <h2>Create bill</h2>
+  <form action="saveBill" method="post">
+  <pre>
+   First Name<input type="text" name="firstName" value ="${contact.firstName}"/>
+   Last Name<input type="text" name="lastName"value ="${contact.lastName}"/>
+   Email<input type="text" name="email"value ="${contact.email}"/>
+   Mobile<input type="text" name="mobile"value ="${contact.mobile}"/>
+   product <input type="submit" name ="product"/>
+   Amount <input type ="text" name ="amount"/>
+   
+          <input type ="submit" value="genertae bill"/>
+  </pre>
+ 
+  
+  
+  </form>
+</body>
+</html>
